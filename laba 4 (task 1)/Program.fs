4c9166220bb4f9c47d218bc179bae0a9beec0240
Rubikon0703 <urasterxov0703@gmail.com>
2026-03-16 17:44:04 +0300

    first commit

---
+++ laba 4 (task 1)/Program.fs
@@ -0,0 +1,76 @@
+﻿open System
+
+type Tree<'T> =
+    | Empty
+    | Node of 'T * Tree<'T> * Tree<'T>
+
+let rec map f tree =
+    match tree with
+    | Empty -> Empty
+    | Node (v, left, right) ->
+        Node (f v, map f left, map f right)
+
+let addCharToStrings ch tree =
+    map (fun s -> s + string ch) tree
+
+let rec insert value tree =
+    match tree with
+    | Empty -> Node (value, Empty, Empty)
+    | Node (v, left, right) ->
+        if value < v then
+            Node (v, insert value left, right)
+        elif value > v then
+            Node (v, left, insert value right)
+        else tree
+
+let fromList values =
+    List.fold (fun acc v -> insert v acc) Empty values
+
+let rec toList tree =
+    match tree with
+    | Empty -> []
+    | Node (v, left, right) ->
+        (toList left) @ [v] @ (toList right)
+
+let print tree =
+    printfn "Содержимое дерева: %A" (toList tree)
+
+let rec readStrings currentList =
+    printf "Введите строку (или пустую для окончания ввода): "
+    match Console.ReadLine () with
+    | "" -> List.rev currentList
+    | s -> readStrings (s :: currentList)
+
+let rec readChar () =
+    printf "Введите один символ для добавления в конец строк: "
+    let input = Console.ReadLine ()
+    if String.IsNullOrEmpty input then
+        printfn "!символ не может быть пустым! Повторите ввод."
+        readChar ()
+    elif input.Length > 1 then
+        printfn ("Предупреждение:введено более одного символа.") 
+        printfn ("Использую первый символ '%c'.") input.[0]
+        input.[0]
+    else
+        input.[0]
+
+[<EntryPoint>]
+let main argv =
+
+    let strings = readStrings []
+
+    if List.isEmpty strings then
+        printfn "Не введено ни одной строки. Выход."
+        0
+    else
+        let ch = readChar ()
+        let originalTree = fromList strings
+        printfn "\nИсходное дерево:"
+        print originalTree
+
+        let newTree = addCharToStrings ch originalTree
+        printfn "\nДерево после добавления символа '%c':" ch
+        print newTree
+
+       
+        0
---
+++ laba 4 (task 2)/Program.fs
@@ -0,0 +1,71 @@
+﻿open System
+
+type Tree<'T> =
+    | Empty
+    | Node of 'T * Tree<'T> * Tree<'T>
+
+let rec insert value tree =
+    match tree with
+    | Empty -> Node (value, Empty, Empty)
+    | Node (v, left, right) ->
+        if value < v then
+            Node (v, insert value left, right)
+        elif value > v then
+            Node (v, left, insert value right)
+        else tree
+
+let fromList values =
+    List.fold (fun acc v -> insert v acc) Empty values
+
+let rec toList tree =
+    match tree with
+    | Empty -> []
+    | Node (v, left, right) ->
+        (toList left) @ [v] @ (toList right)
+
+let print tree =
+    printfn "Содержимое дерева: %A" (toList tree)
+
+let rec foldTree f acc tree =
+    match tree with
+    | Empty -> acc
+    | Node (v, left, right) ->
+        let accLeft = foldTree f acc left
+        let accNode = f v accLeft
+        foldTree f accNode right
+
+let getEvenElements tree =
+    foldTree (fun x acc ->
+        if x % 2 = 0 then x :: acc else acc) [] tree
+    |> List.rev
+
+let rec readInts currentList =
+    printf "Введите целое число (или пустую строку для окончания ввода): "
+    let input = Console.ReadLine ()
+    if String.IsNullOrEmpty input then
+        List.rev currentList
+    else
+        match Int32.TryParse input with
+        | true, num -> readInts (num :: currentList)
+        | false, _ ->
+            printfn "Ошибка: введите корректное целое число."
+            readInts currentList
+
+[<EntryPoint>]
+let main argv =
+
+
+    let numbers = readInts []
+
+    if List.isEmpty numbers then
+        printfn "Не введено ни одного числа. Выход."
+        0
+    else
+        let tree = fromList numbers
+        printfn "\nИсходное дерево:"
+        print tree
+
+        let evens = getEvenElements tree
+        printfn "\nСписок чётных элементов: %A" evens
+
+        0
